7f9a39c9d57a87ab8f19827f4e213550d8f18af9
aymaneiml <aymaneimlihi@gmail.com>
2025-09-21 19:13:45 +0200

    pipeline ci/cd update 2

--- src/test/java/pipeline/tuto/sa/SaAnalysysApplicationTests.java
+++ src/test/java/pipeline/tuto/sa/SaAnalysysApplicationTests.java
@@ -1,13 +1,20 @@
 package pipeline.tuto.sa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SaAnalysysApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    void endpointSaShouldReturnList() {
+        String body = restTemplate.getForObject("/sa", String.class);
+        assertThat(body).contains("Belle formation");
+    }
 }
